bf1a0d5a9bad49afea2ef82dd66dde625a18d04a
vitorhugopassosramalho <vitorhugopassosramalho@gmail.com>
2026-05-11 20:04:09 -0300

    app.py

--- app.py
+++ app.py
@@ -1,15 +1,14 @@
- import streamlit as st
-...
-... st.title("Servidor Web")
-...
-... st.header("Disciplina: Servidores e Computação em Nuvem")
-...
-... st.write("Linguagem: Python.")
-... st.write("Os softwares: Python, Streamlit, GitHub.")
-... st.write("Aluno: Vitor Hugo Passos Ramalho.")
-... st.write("Turma: 2026.1")
-...
-... nome = st.text_input("Digite seu nome:")
-...
-... if nome:
-...     st.write("Olá,", “nome“ , "! O servidor está funcionando.")
+import streamlit as st
+
+st.title("Servidor Web")
+
+st.header("Disciplina: Servidores e Computação em Nuvem")
+
+st.write("Linguagem: Python.")
+st.write("Os softwares: Python, Streamlit, GitHub.")
+st.write("Aluno: Vitor Hugo Passos Ramalho.")
+
+nome = st.text_input("Digite seu nome:")
+
+if nome:
+    st.write("Olá,", nome, "!O Servidor funcionando.")
